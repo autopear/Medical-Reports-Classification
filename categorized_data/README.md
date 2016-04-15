d725d9a6c8bf1df629e7785431dc3d1d869bdea5
Merlin <lonelystars@gmail.com>
2016-04-15 13:37:07 -0400

    Labeled more reports

---
+++ categorized_data/README.md
@@ -0,0 +1,4 @@
+Total:    5470
+None:     2785
+Unknown:  2396
+Seizures: 289
--- categorized_data/genlist.sh
+++ categorized_data/genlist.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
+num=$(find . -name "*.txt" | wc -l | tr -d ' ')
+echo "Total:    $num"
+num=$(find none -name "*.txt" | wc -l | tr -d ' ')
+echo "None:     $num"
+num=$(find unknown -name "*.txt" | wc -l | tr -d ' ')
+echo "Unknown:  $num"
+num=$(find seizures -name "*.txt" | wc -l | tr -d ' ')
+echo "Seizures: $num"
 ls none/*.txt > none.list
-ls seizures > seizures.list
+ls seizures/*.txt > seizures.list
